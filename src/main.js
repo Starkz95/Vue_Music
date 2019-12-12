bec56e5d1f73a32faa1c30bb3b64350395f14fe2
zmc <834176915@qq.com>
2019-12-12 16:17:29 +0800

    modules finish

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router.js'
+import store from './store.js'
+
+Vue.use(store)
+// axios
+import axios from 'axios'
+Vue.prototype.axios = axios;
+axios.defaults.baseURL = "http://musicapi.leanapp.cn";  //这里使用自己下载的网易云接口地址
+
+// 引入Vant
+import Vant from 'vant'
+import 'vant/lib/index.css';
+import { Lazyload } from 'vant';
+Vue.use(Vant)
+Vue.use(Lazyload);
+
+// animation动画库
+import animate from 'animate.css'
+Vue.use(animate);
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
